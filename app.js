1622207b97ed15bd59cf8eeaf0b195c536c0adfd
rammy81 <john.ramsbottom1@gmail.com>
2023-03-26 22:47:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -71,78 +71,38 @@ function createInteractiveBarChart() {
 
 createInteractiveBarChart();
 
-function createInteractiveBarChart() {
-  // Your D3 code to create the interactive bar chart with a tooltip
 
+function createVisualization() {
+  const data = [4, 8, 15, 16, 23, 42];
 
-// Call the function to create the bar chart
-create400px();
+  const width = 420;
+  const barHeight = 20;
 
-const data = [
-  { label: 'A', value: 10 },
-  { label: 'B', value: 15 },
-  { label: 'C', value: 20 },
-  { label: 'D', value: 25 },
-  { label: 'E', value: 30 },
-];
+  const x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
 
-const width = 400;
-const height = 200;
-const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+  const chart = d3.select("#bar2")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
 
-const x = d3.scaleBand()
-  .domain(data.map(d => d.label))
-  .range([margin.left, width - margin.right])
-  .padding(0.1);
+  const bar = chart.selectAll("g")
+    .data(data)
+    .enter().append("g")
+      .attr("transform", (d, i) => `translate(0,${i * barHeight})`);
 
-const y = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.value)]).nice()
-  .range([height - margin.bottom, margin.top]);
+  bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
 
-const xAxis = g => g
-  .attr("transform", `translate(0,${height - margin.bottom})`)
-  .call(d3.axisBottom(x).tickSizeOuter(0));
-
-const yAxis = g => g
-  .attr("transform", `translate(${margin.left},0)`)
-  .call(d3.axisLeft(y))
-  .call(g => g.select(".domain").remove());
-
-const tooltip = d3.select("#tooltip");
-
-const svg = d3.select("#fourhun").append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-svg.append("g")
-  .attr("fill", "steelblue")
-  .selectAll("rect")
-  .data(data)
-  .join("rect")
-    .attr("x", d => x(d.label))
-    .attr("y", d => y(d.value))
-    .attr("height", d => y(0) - y(d.value))
-    .attr("width", x.bandwidth())
-    .on("mouseover", (event, d) => {
-      tooltip.style("opacity", 1)
-        .html(`Label: ${d.label}<br>Value: ${d.value}`)
-        .style("left", (event.pageX + 10) + "px")
-        .style("top", (event.pageY - 10) + "px");
-    })
-    .on("mousemove", (event, d) => {
-      tooltip.style("left", (event.pageX + 10) + "px")
-        .style("top", (event.pageY - 10) + "px");
-    })
-    .on("mouseout", () => {
-      tooltip.style("opacity", 0);
-    });
-
-svg.append("g")
-  .call(xAxis);
-
-svg.append("g")
-  .call(yAxis);
+  bar.append("text")
+    .attr("x", d => x(d) - 3)
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(d => d);
 }
 
-// Call the function to create the bar chart
-create400px();
+
+
+createVisualization();
