a2576006279eee103c4b877daafbe63d6722b4af
rammy81 <john.ramsbottom1@gmail.com>
2023-03-26 19:14:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-function createVisualization() {
-  const data = [4, 8, 15, 16, 23, 42];
-
-  const width = 420;
-  const barHeight = 20;
-
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
-  const chart = d3.select("#firstbar")
-    .append("svg")
-      .attr("width", width)
-      .attr("height", barHeight * data.length);
-
-  const bar = chart.selectAll("g")
-    .data(data)
-    .enter().append("g")
-      .attr("transform", (d, i) => `translate(0,${i * barHeight})`);
-
-  bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-  bar.append("text")
-    .attr("x", d => x(d) - 3)
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(d => d);
-}
-
-
 function createInteractiveBarChart() {
   const data = [4, 8, 15, 16, 23, 42];
 
@@ -40,7 +8,7 @@ function createInteractiveBarChart() {
     .domain([0, d3.max(data)])
     .range([0, width]);
 
-  const chart = d3.select("#2ndBar")
+  const chart = d3.select("#visualization")
     .append("svg")
       .attr("width", width)
       .attr("height", barHeight * data.length);
@@ -67,5 +35,4 @@ function createInteractiveBarChart() {
     .text(d => d);
 }
 
-createVisualization();
 createInteractiveBarChart();
