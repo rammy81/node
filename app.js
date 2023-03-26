819a7383fce6474aede88c6fc465ab219bf94832
rammy81 <john.ramsbottom1@gmail.com>
2023-03-26 18:26:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,79 +1,32 @@
-function createVisualization1() {
-<script>
+function createVisualization() {
+  const data = [4, 8, 15, 16, 23, 42];
 
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 30, left: 30},
-  width = 450 - margin.left - margin.right,
-  height = 450 - margin.top - margin.bottom;
+  const width = 420;
+  const barHeight = 20;
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+  const x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
 
-// Labels of row and columns
-var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"]
+  const chart = d3.select("#visualization")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
 
-// Build X scales and axis:
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(myGroups)
-  .padding(0.01);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
+  const bar = chart.selectAll("g")
+    .data(data)
+    .enter().append("g")
+      .attr("transform", (d, i) => `translate(0,${i * barHeight})`);
 
-// Build X scales and axis:
-var y = d3.scaleBand()
-  .range([ height, 0 ])
-  .domain(myVars)
-  .padding(0.01);
-svg.append("g")
-  .call(d3.axisLeft(y));
+  bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
 
-// Build color scale
-var myColor = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
-  .domain([1,100])
-
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
-
-  svg.selectAll()
-      .data(data, function(d) {return d.group+':'+d.variable;})
-      .enter()
-      .append("rect")
-      .attr("x", function(d) { return x(d.group) })
-      .attr("y", function(d) { return y(d.variable) })
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.value)} )
-
-})
-
-</script>
-    const svg = d3.select("#visualization1").append("svg")
-        // Set the width, height, and other attributes for the SVG element
-        .attr("width", width)
-        .attr("height", height);
-
-    // Add your D3.js code for creating the first visualization here
+  bar.append("text")
+    .attr("x", d => x(d) - 3)
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(d => d);
 }
 
-function createVisualization2() {
-    // D3.js code for the second visualization
-    const svg = d3.select("#visualization2").append("svg")
-        // Set the width, height, and other attributes for the SVG element
-        .attr("width", width)
-        .attr("height", height);
-
-    // Add your D3.js code for creating the second visualization here
-}
-
-
-createVisualization1();
+createVisualization();
